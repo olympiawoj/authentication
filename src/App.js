1ad747ac3ca51530a90e088418cba24738b6db46
olympiawoj <owojcik7@gmail.com>
2021-03-01 18:38:35 -0500

    Starting with the Auth0 SPA SDK

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SiteHeader from './components/SiteHeader';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import './App.css';
+import createAuth0Client from '@auth0/auth0-spa-js';
 
 export default function App() {
+  
+  useEffect(()=>{
+
+    initAuth0()
+
+    async function initAuth0(){
+      const auth0 = await createAuth0Client({
+        domain: 'dev-2g4o78n1.us.auth0.com',
+        client_id: '4XA9X9jCVzYeG9zaO5SAn2mRCC0omVO0'
+      });
+
+      const isAuthenticated = await auth0.isAuthenticated();
+      const user = await auth0.getUser()
+      console.log(isAuthenticated)
+      console.log(user)
+    }
+
+  },[])
+
   return (
     <Router>
       <div className="app">
